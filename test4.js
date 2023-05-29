4771be6d56e7c47b882601a7f42c3196752dc399
bswiley <bswiley@gmail.com>
2023-05-29 16:07:23 -0500

    add test4.js

--- test4.js
+++ test4.js
@@ -1,4 +1,17 @@
-db.query('SELECT CONCAT(id," - ",name) AS department FROM department"', function (err, results){
+const inquirer = require("inquirer");
+const sql = require("mysql2");
+const db = sql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: '4wm58f6t',
+      database: 'personel_db'
+    },
+    console.log(`Connected to the personel_db database.`));
+
+db.query('SELECT * FROM department'), function (err, results){
     console.log(results);
     console.table(results);
 }
